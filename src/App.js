d0f74d569bf29f1c7f843192c45f5b5a6a149b0c
Ziya <47672872+SafSafir@users.noreply.github.com>
2021-08-04 13:01:15 +0300

    test7

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Hero from "./components/Hero";
-import ErrorBoundary from "./components/ErrorBoundary";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 function App() {
   return (
     <div className='App'>
-      <ErrorBoundary>
-        <Hero heroName='Batman'></Hero>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName='Superman'></Hero>
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName='Joker'></Hero>
-      </ErrorBoundary>
+      <ClickCounter name='Sikis Ustasi'></ClickCounter>
+      <HoverCounter></HoverCounter>
     </div>
   );
 }
